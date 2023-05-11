98b6b4a036b0fd4d9e6d707f7e7fcfb0865e3cce
Tanner Bebb <17tbebb@gmail.com>
2023-05-11 08:53:24 -0700

    - Cleaned up the output of the code - Added a do while loop to allow for repeat the program

--- ArrayforInventory/inventory.cpp
+++ ArrayforInventory/inventory.cpp
@@ -1,5 +1,4 @@
 /*Next objectives:
-add an if else statement asking the user if they would want to sell or use items
 add in a way to allow users to add an item to the current inventory
 note: this might be easier if I use a linked list, I might come back to this idea.*/
 
@@ -17,39 +16,47 @@ int main() {
 void inventorySys() {
 	string items[3];
 	string number;
-	char response;
+	char responseA, Repeat;
+	do {
 
-	//for loop that stores items in the list array
-	cout << "Please enter the items that you are storing in your inventory: " << endl;
-	for (int i = 0; i < 3; i++) {
-		cin >> items[i];
-	}
-
-
-	//if statement that asks the user if they want to use or sell anything
-	cout << "Would you like to use or sell and item(Y or N)?" << endl;
-	cin >> response;
-	if (response == 'Y' || response == 'y') {
-		//asking the user for which item they would like to delete
-		cout << "Enter the item that you are selling or using: " << endl;
-		cin >> number;
+		//for loop that stores items in the list array
+		cout << "Please enter the items that you are storing in your inventory: " << endl;
+		for (int i = 0; i < 3; i++) {
+			cin >> items[i];
+		}
 		cout << endl;
 
-		cout << "The items that are left in you inventory are: " << endl;
-		//for loop to delet an item from the array
-		for (int i = 0; i < 3; i++) {
-			if (items[i] != number) {
+		//if statement that asks the user if they want to use or sell anything
+		cout << "Would you like to use or sell and item(Y or N)?" << endl;
+		cin >> responseA;
+		cout << endl;
+		if (responseA == 'Y' || responseA == 'y') {
+			//asking the user for which item they would like to delete
+			cout << "Enter the item that you are selling or using: " << endl;
+			cin >> number;
+			cout << endl;
+
+			cout << "Your current inventory is: " << endl;
+			//for loop to delet an item from the array
+			for (int i = 0; i < 3; i++) {
+				if (items[i] != number) {
+					cout << items[i] << endl;
+				}
+			}
+		}
+
+		else {
+			cout << "Your current inventory is: " << endl;
+
+			for (int i = 0; i < 3; i++) {
 				cout << items[i] << endl;
 			}
 		}
-	}
 
-	else {
-		cout << "Your current inventory is: " << endl;
+	
+		cout << endl << "Would you like to run it again(Y or N): " << endl;
+		cin >> Repeat;
+		cout << endl;
 
-		for (int i = 0;i < 3;i++) {
-			cout << items[i] << endl;
-		}
-	}
-
+	} while (Repeat == 'Y' || Repeat == 'y');
 }
